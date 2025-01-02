d624f8cb2d937e0ff266a1771dbc3a0c56208d60
karthik_raj <raj.karthik5566@gmail.com>
2025-01-02 09:51:53 +0530

    minor fix

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-      origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // Replace with your frontend URL
       credentials: true, // Allow credentials (cookies, headers)
   })
 );
