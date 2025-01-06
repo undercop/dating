a138c9b850bb826d78cd05136a4fffe11baa2ec1
karthik_raj <raj.karthik5566@gmail.com>
2025-01-06 10:47:07 +0530

    added production url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://cupid-8quv.onrender.com/api";
 
 export const axiosInstance = axios.create({
 	baseURL: BASE_URL,
