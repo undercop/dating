1cef5d5752ce0cc7090bf867e0d6b10e3c801032
karthik_raj <raj.karthik5566@gmail.com>
2025-01-10 21:05:29 +0530

    add links

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: process.env.CLIENT_URL,
+		origin: "https://cupid-accf.onrender.com/",
 		credentials: true,
 	})
 );
