8d4be54dd0417f0c860baf4876982fc3be796fd1
karthik_raj <raj.karthik5566@gmail.com>
2025-07-01 19:43:54 +0530

    finallyyyy fixed the postcss and tailwind css bug

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,14 +2,12 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  
-    mode: 'jit',
-    content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      daisyui
-    ],
-  }
-  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
